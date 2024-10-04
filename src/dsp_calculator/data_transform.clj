d7ebac562afe029198d4622d103ce007592135dd
Zefira Shannon <earthlingzephyr@gmail.com>
2024-10-03 20:57:00 -0700

    Transform stringified BigInts to numbers in edn files

--- src/dsp_calculator/data_transform.clj
+++ src/dsp_calculator/data_transform.clj
@@ -11,6 +11,20 @@
 
 (def files ["locale" "items" "meta" "recipes" "tech"])
 
+(defn type-map? [item]
+  (and (map? item)
+       (= #{":type" "value"} (set (keys item)))))
+
+(defn simplify [{type ":type" value "value" :as item}]
+  (if (= type "BigInt")
+    (edn/read-string value)
+    item))
+
+(defn revive [item]
+  (if (type-map? item)
+    (simplify item)
+    item))
+
 (defn main []
   (let [locales (with-open [reader (io/reader (io/resource (str root-path "locale.json")))]
                   (json/read reader))
@@ -20,7 +34,7 @@
       (with-open [reader (io/reader (io/resource (str root-path file ".json")))
                   writer (io/writer (io/file (io/resource root-path) (str file ".edn")))
                   writer-en (io/writer (io/file (io/resource root-path) (str file "_EN.edn")))]
-        (let [json (json/read reader)
+        (let [json (walk/prewalk revive (json/read reader))
               json-en (walk/prewalk-replace en-strings json)]
           (pp/pprint json writer)
           (pp/pprint json-en writer-en))))))
