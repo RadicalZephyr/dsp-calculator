5ff970086b5cf92e141a2bd6406c51454e552f6d
Zefira Shannon <earthlingzephyr@gmail.com>
2024-10-03 21:49:41 -0700

    Add the spritesheets icon demo to a devcard

--- test/dsp_calculator/devcards.cljs
+++ test/dsp_calculator/devcards.cljs
@@ -1,7 +1,8 @@
 (ns dsp-calculator.devcards
   (:require
     [devcards.core]
-    [dsp-calculator.devcards.core]))
+    [dsp-calculator.devcards.core]
+    [dsp-calculator.devcards.icons]))
 
 (defn ^:export main []
   (devcards.core/start-devcard-ui!))
