4bb3f7826f64245093bc459d94ff726545cb7b25
Zefira Shannon <earthlingzephyr@gmail.com>
2024-10-20 01:47:08 -0700

    Include css from dsp-ratios

--- test/dsp_calculator/devcards/calculator.cljs
+++ test/dsp_calculator/devcards/calculator.cljs
@@ -6,3 +6,20 @@
   "# Calculator
 
 The calculator interface, the most important part of the site.")
+
+(defcard-rg css
+  "This includes the css for the calculator interface."
+  [:div
+   [:link {:rel "stylesheet" :href "/css/reset.css"}]
+   [:link {:rel "stylesheet" :href "/css/variables.css"}]
+   [:link {:rel "stylesheet" :href "/css/cursor.css"}]
+   [:link {:rel "stylesheet" :href "/css/fonts.css"}]
+   [:link {:rel "stylesheet" :href "/css/icons.css"}]
+   [:link {:rel "stylesheet" :href "/css/pages/calculator.css"}]
+   [:link {:rel "stylesheet" :href "/css/components/combo-selector.css"}]])
+
+(defcard-rg combo-selector
+  [:div.combo-selector
+   [:div.recipe-picker
+    [:div.icon {:data-icon "ui.select-recipe" :title "Select a recipe"}]
+    [:span.hint "Please select a recipe"]]])
