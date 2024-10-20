5d3d8c033e52d8768d66f21fdb85a30cfcbf1839
Zefira Shannon <earthlingzephyr@gmail.com>
2024-10-19 23:22:17 -0700

    Merge dual description fields instead of losing data

--- src/dsp_calculator/data_transform.clj
+++ src/dsp_calculator/data_transform.clj
@@ -25,15 +25,28 @@
 (defn kebab-keywordify [k]
   [k (keyword (csk/->kebab-case k))])
 
-(defn cljify-map [m]
-  (let [smap (into {} (map kebab-keywordify (keys m)))]
-    (set/rename-keys m smap)))
+(defn merge-desc-fields [m]
+  (if (and (contains? m "descFields")
+           (contains? m "_descFields"))
+    (let [num-d-fields (get m "descFields")
+          map-d-fields (get m "_descFields")
+          desc-fields (vec (map #(assoc %1 "position" %2)
+                                map-d-fields
+                                num-d-fields))]
+      (-> m
+          (dissoc "descFields" "_descFields")
+          (assoc "descFields" desc-fields)))
+    m))
 
-(defn revive [item]
+(defn cljify-map [m]
+  (let [m (merge-desc-fields m)
+        smap (into {} (map kebab-keywordify (keys m)))]
+    (into (sorted-map) (set/rename-keys m smap))))
+
+(defn transform [item]
   (cond (type-map? item) (simplify item)
         (map? item) (cljify-map item)
         :else item))
-
 
 (defn main []
   (let [locales (with-open [reader (io/reader (io/resource (str root-path "locale.json")))]
@@ -46,7 +59,7 @@
                                          (io/resource
                                           (str root-path filename ".json")))]
                        (json/read reader))
-            json (walk/postwalk revive raw-json)
+            json (walk/prewalk transform raw-json)
             json-en (walk/prewalk-replace en-strings json)]
         (with-open [writer (io/writer
                             (io/file
