9449294747c6feca09d5f5dd8d73ade92512df37
Zefira Shannon <earthlingzephyr@gmail.com>
2024-10-21 00:27:11 -0700

    Implement tab switching using local state

--- src/dsp_calculator/ui/calculator.cljs
+++ src/dsp_calculator/ui/calculator.cljs
@@ -1,5 +1,6 @@
 (ns dsp-calculator.ui.calculator
-  (:require [spade.core :refer [defclass]]))
+  (:require [spade.core :refer [defclass]]
+            [reagent.core :as reagent]))
 
 (declare combo-selector)
 
@@ -23,30 +24,42 @@
       [recipe-icon item]])])
 
 (defn recipe-picker [items buildings]
-  [:dialog.window.recipes {:open true
-                           :style {:position "relative"}}
-   [:header "Select a Recipe"]
-   [:div.tablist {:role "tablist"}
-    [:button#tab-0.tab {:type "button" :role "tab" :tabIndex "0"}
-     "Items"]
-    [:button#tab-1.tab {:type "button" :role "tab" :tabIndex "-1"}
-     "Buildings"]]
-   [:div#tabpanel-0.tabpanel.is-visible {:role "tabpanel"}
-    [:ul.recipe-grid {:role "listbox"}
-     (for [{[x y] :pos :as item} items]
-       ^{:key item}
-       [:li {:role "option"
-             :class (grid-pos x y)}
-        [recipe-icon item]])]]
-   [:div#tabpanel-1.tabpanel.is-hidden {:role "tabpanel"}]
-   [:div.corner-nav
-    [:button.close]]])
+  (let [first-tab? (reagent/atom true)
+        first-tab (fn [] (reset! first-tab? true))
+        second-tab (fn [] (reset! first-tab? false))]
+    (fn [items buildings]
+      (let [first-tab? @first-tab?]
+       [:dialog.window.recipes {:open true
+                                :style {:position "relative"}}
+        [:header "Select a Recipe"]
+        [:div.tablist {:role "tablist"}
+         [:button#tab-0.tab {:type "button"
+                             :role "tab"
+                             :on-click first-tab
+                             :aria-selected (str first-tab?)
+                             :aria-controls "tabpanel-0"}
+          "Items"]
+         [:button#tab-1.tab {:type "button"
+                             :role "tab"
+                             :on-click second-tab
+                             :aria-selected (str (not first-tab?))
+                             :aria-controls "tabpanel-1"}
+          "Buildings"]]
+        [:div#tabpanel-0.tabpanel
+         {:role "tabpanel"
+          :class [(if first-tab? "is-visible" "is-hidden")]}
+         [recipe-grid items]]
+        [:div#tabpanel-1.tabpanel.
+         {:role "tabpanel"
+          :class [(if (not first-tab?) "is-visible" "is-hidden")]}
+         [recipe-grid buildings]]
+        [:div.corner-nav
+         [:button.close]]]))))
 
 (defn empty-selector []
   [:div.recipe-picker
    [:div.icon {:data-icon "ui.select-recipe" :title "Select a recipe"}]
    [:span.hint "Please select a recipe"]])
-
 
 (defn selected-recipe [selected]
   [recipe-icon selected])
